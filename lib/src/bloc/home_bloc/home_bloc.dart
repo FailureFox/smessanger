be9deed4992fd292f77e58e69c4192a974b51070
FailureFox <loxchmo123@gmail.com>
2021-12-30 16:57:05 +0500

    no comment

--- lib/src/bloc/home_bloc/home_bloc.dart
+++ lib/src/bloc/home_bloc/home_bloc.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smessanger/src/bloc/home_bloc/home_event.dart';
 import 'package:smessanger/src/bloc/home_bloc/home_state.dart';
+import 'package:smessanger/src/models/my_profile_model.dart';
+import 'package:smessanger/src/resources/domain/repositories/firebase_repository.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(HomeState()) {
+  final FireBaseRepository fRepository;
+  HomeBloc({required this.fRepository}) : super(HomeState()) {
+    on<HomeLoadingEvent>((event, emit) {
+      fRepository
+          .getMyUser(event.uid)
+          .listen((event) => myUserChangedEvent(event));
+    });
+
     on<HomePageChangeEvent>(
         (event, emit) => emit(state.copyWith(page: event.page)));
+  }
+  myUserChangedEvent(MyProfile myProfile) {
+    emit(state.copyWith(myProfile: myProfile));
   }
 }
