03e6f93478c46ece7a4926a7c0b3cc76e0f0f684
FailureFox <loxchmo123@gmail.com>
2022-01-07 00:05:24 +0500

    theme

--- lib/src/bloc/app_bloc/app_bloc.dart
+++ lib/src/bloc/app_bloc/app_bloc.dart
@@ -13,7 +13,8 @@ class AppBloc extends Bloc<AppEvents, AppState> {
 // storage.getBool('theme')
     on<AppThemeLoadingEvent>((event, emit) async {
       storage = await SharedPreferences.getInstance();
-      emit(state.copyWith(isDark: false));
+      final theme = storage.getBool('theme') ?? false;
+      emit(state.copyWith(isDark: theme));
       final token = await _secureStorage.read(key: 'token');
       await Future.delayed(const Duration(seconds: 2));
 
